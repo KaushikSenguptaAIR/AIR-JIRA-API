40e978a6be8134f7ea6a6563faea6547b7674e38
KaushikSenguptaAIR <kaushik.sengupta@air.global>
2026-04-10 19:39:04 +0530

    Changed JIRA Baser url to check if the connection is working fine

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def debug_jira():
         credentials = base64.b64encode(f"{JIRA_USERNAME}:{JIRA_TOKEN}".encode()).decode()
         
         # Test basic JIRA access
-        url = f"{JIRA_BASE_URL}/rest/api/3/issue/DITSD-322"
+        url = f"{JIRA_BASE_URL}/rest/servicedeskapi/servicedesk/76"
         
         headers = {
             'Authorization': f'Basic {credentials}',
